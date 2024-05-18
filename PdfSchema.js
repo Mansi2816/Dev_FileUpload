07857325e5e73aa58a1b8f337ebb7540530d5f8d
Mansi Soni <143398274+Mansi2816@users.noreply.github.com>
2024-05-18 17:05:27 +0530

    Create PdfSchema.js

---
+++ PdfSchema.js
@@ -0,0 +1,10 @@
+const mongoose = require ('mongoose')
+
+const pdfDetailsSchema = new mongoose.Schema({
+    pdf:String,
+    title: String,    
+},{collection:"pdfDetails"}
+)
+
+mongoose.model("pdfDetails", pdfDetailsSchema)
+
